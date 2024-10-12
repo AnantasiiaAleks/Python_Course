2bd3606fcd6bad461509bfb76f610e3ba1d4d704
AnastasiiaAleks <chiffka@gmail.com>
2024-10-13 00:17:41 +0300

    fixed google_colab_examples

--- Examples/pandas_seaborn/one_hot.py
+++ Examples/pandas_seaborn/one_hot.py
@@ -20,9 +20,9 @@ random.shuffle(lst)
 print(lst)
 print()
 data = pd.DataFrame({'whoAmI':lst})
-data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
-data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
-data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
-data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = 1
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = 0
+data.loc[data['whoAmI'] == 'human', 'human_group'] = 1
+data.loc[data['whoAmI'] != 'human', 'human_group'] = 0
 
 data.head(n=20)
